35092335b45ad2f5013a0be6a06e72735a41f7a3
K <1257868332@qq.com>
2020-01-26 16:50:37 +0800

    add check if input field is empty

--- js/function.js
+++ js/function.js
@@ -1,5 +1,8 @@
-$(document).ready(
-	function submit(){
-		var button = document.getElementById('submitButton');
+function check(){//检查文本框是否为空
+	var o = document.getElementById("inputText");
+	var v = o.value;
+	v = v.replace(/[ ]/g,""); 
+	if(v == ''){
+	return false;
 	}
-);
+}

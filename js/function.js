db952106a32c5910f46043a69f283e4c513f5660
K <1257868332@qq.com>
2020-01-27 22:45:29 +0800

    finish search engine,add labels,add fade-in effect

--- js/function.js
+++ js/function.js
@@ -29,75 +29,75 @@ function select(){
 	if ($('#folder').css('display') == 'block') {
 		$('#folder').css('display', 'none');
         $('#nav').css('display', 'block');
-        document.getElementById("Select").innerHTML = "导航 <img src='img/search-change.svg?v=2ae7ab8'>";
+        document.getElementById("Select").innerHTML = "<hr>导航 <img src='img/search-change.svg?v=2ae7ab8'>";
     }else if ($('#nav').css('display') == 'block'){
         $('#nav').css('display', 'none');
         $('#folder').css('display', 'block');
-        document.getElementById("Select").innerHTML = "标签 <img src='img/search-change.svg?v=2ae7ab8'>";
+        document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
     }
 }
 
 // 切换搜索引擎
 function baidu(){
-	document.getElementById("state").innerHTML = "<img style='width:300px;' src='img/baidu.png'>";
+	document.getElementById("state").innerHTML = "<img style='width:300px;' src='https://s2.ax1x.com/2020/01/27/1ugwKf.md.png'>";
 	$('#submitButton').css('background-color', '#3245df');
 	$('#Select').css('color', '#3245df');
 	$('span').css('background-color', '#3245df');
     $('#nav').css('display', 'none');
     $('#folder').css('display', 'block');
-    document.getElementById("Select").innerHTML = "标签 <img src='img/search-change.svg?v=2ae7ab8'>";
+    document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
     $("#inputText").attr("placeholder","百度一下...");
     $("#form").attr("action","https://www.baidu.com/s");
     $("#inputText").attr("name","wd");
 }
 
 function google(){
-	document.getElementById("state").innerHTML = "<img style='width:300px;' src='img/google.png'>";
+	document.getElementById("state").innerHTML = "<img style='width:300px;' src='https://s2.ax1x.com/2020/01/27/1ugy5j.md.png'>";
 	$('#submitButton').css('background-color', '#4285f4');
 	$('#Select').css('color', '#4285f4');
 	$('span').css('background-color', '#4285f4');
     $('#nav').css('display', 'none');
     $('#folder').css('display', 'block');
-    document.getElementById("Select").innerHTML = "标签 <img src='img/search-change.svg?v=2ae7ab8'>";
+    document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
     $("#inputText").attr("placeholder","咕噜咕噜...");
     $("#form").attr("action","https://www.google.com/search");
     $("#inputText").attr("name","q");
 }
 
 function bing(){
-	document.getElementById("state").innerHTML = "<img style='width:300px;' src='img/bing.png'>";
+	document.getElementById("state").innerHTML = "<img style='width:300px;' src='https://s2.ax1x.com/2020/01/27/1ugtPA.md.png'>";
 	$('#submitButton').css('background-color', '#00868B');
 	$('#Select').css('color', '#00868B');
 	$('span').css('background-color', '#00868B');
     $('#nav').css('display', 'none');
     $('#folder').css('display', 'block');
-    document.getElementById("Select").innerHTML = "标签 <img src='img/search-change.svg?v=2ae7ab8'>";
+    document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
     $("#inputText").attr("placeholder","Bing搜索...");
     $("#form").attr("action","https://cn.bing.com/search");
     $("#inputText").attr("name","q");
 }
 
 function yahoo(){
-	document.getElementById("state").innerHTML = "<img style='width:300px;' src='img/yahoo.png'>";
+	document.getElementById("state").innerHTML = "<img style='width:300px;' src='https://s2.ax1x.com/2020/01/27/1ughrT.md.png'>";
 	$('#submitButton').css('background-color', '#5f01d1');
 	$('#Select').css('color', '#5f01d1');
 	$('span').css('background-color', '#5f01d1');
     $('#nav').css('display', 'none');
     $('#folder').css('display', 'block');
-    document.getElementById("Select").innerHTML = "标签 <img src='img/search-change.svg?v=2ae7ab8'>";
+    document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
     $("#inputText").attr("placeholder","Yahoo~");
     $("#form").attr("action","https://search.yahoo.com/search");
     $("#inputText").attr("name","p");
 }
 
 function duckduckgo(){
-	document.getElementById("state").innerHTML = "<img style='width:300px;' src='img/duckduckgo.png'>";
+	document.getElementById("state").innerHTML = "<img style='width:300px;' src='https://s2.ax1x.com/2020/01/27/1ug0r8.md.png'>";
 	$('#submitButton').css('background-color', '#de5833');
 	$('#Select').css('color', '#de5833');
 	$('span').css('background-color', '#de5833');
     $('#nav').css('display', 'none');
     $('#folder').css('display', 'block');
-    document.getElementById("Select").innerHTML = "标签 <img src='img/search-change.svg?v=2ae7ab8'>";
+    document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
     $("#inputText").attr("placeholder","嘎嘎嘎...");
     $("#form").attr("action","https://duckduckgo.com/");
     $("#inputText").attr("name","q");

024c58d6c94ac863f83757e76911df1a29b002ce
K <1257868332@qq.com>
2020-01-27 15:10:26 +0800

    add labels

--- js/function.js
+++ js/function.js
@@ -1,8 +1,25 @@
-function check(){//检查文本框是否为空
+//检查文本框是否为空
+function check(){
 	var o = document.getElementById("inputText");
 	var v = o.value;
 	v = v.replace(/[ ]/g,""); 
 	if(v == ''){
 	return false;
 	}
+}
+
+//title问候语
+var d = new Date()
+var time = d.getHours()
+if (time < 24){ 
+document.getElementById("title").innerHTML="一个导航 | Good evening";
+}
+if (time < 19){ 
+document.getElementById("title").innerHTML="一个导航 | Good afternoon";
+} 
+if (time < 12){ 
+document.getElementById("title").innerHTML="一个导航 | Good morning";
+} 
+if (time < 5){
+ document.getElementById("title").innerHTML="一个导航 | Stay up late again";
 }

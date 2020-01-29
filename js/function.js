673f2906a7e0421673d3ecd64797bbf38cc85ab7
K <1257868332@qq.com>
2020-01-30 00:02:31 +0800

    add sideBar,night mode,v2.0.0 the first version

--- js/function.js
+++ js/function.js
@@ -1,165 +1,204 @@
 //检查文本框是否为空
-function check(){
+function check() {
 	var o = document.getElementById("inputText");
 	var v = o.value;
-	v = v.replace(/[ ]/g,""); 
-	if(v == ''){
-	return false;
+	v = v.replace(/[ ]/g, "");
+	if (v == '') {
+		return false;
 	}
 }
 
 //title问候语
 var d = new Date()
 var time = d.getHours()
-if (time < 24){ 
-document.getElementById("title").innerHTML="一个导航 | Good evening";
+if (time < 24) {
+	document.getElementById("title").innerHTML = "一个导航 | Good evening";
 }
-if (time < 19){ 
-document.getElementById("title").innerHTML="一个导航 | Good afternoon";
-} 
-if (time < 12){ 
-document.getElementById("title").innerHTML="一个导航 | Good morning";
-} 
-if (time < 5){
- document.getElementById("title").innerHTML="一个导航 | Stay up late again";
+if (time < 18) {
+	document.getElementById("title").innerHTML = "一个导航 | Good afternoon";
+}
+if (time < 12) {
+	document.getElementById("title").innerHTML = "一个导航 | Good morning";
+}
+if (time < 5) {
+	document.getElementById("title").innerHTML = "一个导航 | Stay up late again";
+}
+
+// 夜间模式
+function night() {
+	if (document.bgColor == "#fff") {
+		document.bgColor = "#2f2f33";
+		document.getElementById("night").innerHTML = "日间模式";
+		document.getElementById("mySidenav").style.backgroundColor = "#6E6E6E";
+		document.getElementById("mySidenav").style.boxShadow="10px 0 5px #6E6E6E";
+	} else if (document.bgColor == "#2f2f33") {
+		document.bgColor = "#fff";
+		document.getElementById("night").innerHTML = "夜间模式";
+		document.getElementById("mySidenav").style.backgroundColor = "#EDEDED";
+		document.getElementById("mySidenav").style.boxShadow="10px 0 5px #EDEDED";
+	}
 }
 
 //导航、引擎选择器
-function select(){
+function select() {
 	if ($('#folder').css('display') == 'block') {
 		$('#folder').css('display', 'none');
-        $('#nav').css('display', 'block');
-        document.getElementById("Select").innerHTML = "<hr>导航 <img src='img/search-change.svg?v=2ae7ab8'>";
-    }else if ($('#nav').css('display') == 'block'){
-        $('#nav').css('display', 'none');
-        $('#folder').css('display', 'block');
-        document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
-    }
+		$('#nav').css('display', 'block');
+		document.getElementById("Select").innerHTML = "<hr>导航 <img src='img/search-change.svg?v=2ae7ab8'>";
+	} else if ($('#nav').css('display') == 'block') {
+		$('#nav').css('display', 'none');
+		$('#folder').css('display', 'block');
+		document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
+	}
+}
+
+// 侧栏
+function Slider() {
+	var width = $('#mySidenav').outerWidth(); //第一次获取#mySidenav的宽
+	if (width == 0) {
+		openSlider();
+	}
+	width = $('#mySidenav').outerWidth(); //重新获取#mySidenav的宽
+	if (width > 0) {
+		closeSlider();
+	}
+}
+
+function openSlider() {
+	document.getElementById("slider").style.left = "83%";
+	document.getElementById("mySidenav").style.width = "78%";
+	document.getElementById("sliderImg").style.transform = "rotateY(180deg)";
+}
+
+function closeSlider() {
+	document.getElementById("slider").style.left = "10px";
+	document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("sliderImg").style.transform = "rotateX(180deg)";
 }
 
 // 切换搜索引擎
-function baidu(){
+function baidu() {
 	document.getElementById("state").innerHTML = "<img style='width:300px;' src='https://s2.ax1x.com/2020/01/27/1ugwKf.md.png'>";
 	$('#submitButton').css('background-color', '#3245df');
 	$('#submitButton').css('color', 'white');
 	$('#Select').css('color', '#3245df');
 	$('span').css('background-color', '#3245df');
-    $('#nav').css('display', 'none');
-    $('#folder').css('display', 'block');
-    document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
-    $("#inputText").attr("placeholder","百度一下...");
-    $("#form").attr("action","https://www.baidu.com/s");
-    $("#inputText").attr("name","wd");
+	$('#nav').css('display', 'none');
+	$('#folder').css('display', 'block');
+	document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
+	$("#inputText").attr("placeholder", "百度一下...");
+	$("#form").attr("action", "https://www.baidu.com/s");
+	$("#inputText").attr("name", "wd");
 }
 
-function google(){
+function google() {
 	document.getElementById("state").innerHTML = "<img style='width:300px;' src='https://s2.ax1x.com/2020/01/27/1ugy5j.md.png'>";
 	$('#submitButton').css('background-color', '#4285f4');
 	$('#submitButton').css('color', 'white');
 	$('#Select').css('color', '#4285f4');
 	$('span').css('background-color', '#4285f4');
-    $('#nav').css('display', 'none');
-    $('#folder').css('display', 'block');
-    document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
-    $("#inputText").attr("placeholder","咕噜咕噜...");
-    $("#form").attr("action","https://www.google.com/search");
-    $("#inputText").attr("name","q");
+	$('#nav').css('display', 'none');
+	$('#folder').css('display', 'block');
+	document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
+	$("#inputText").attr("placeholder", "咕噜咕噜...");
+	$("#form").attr("action", "https://www.google.com/search");
+	$("#inputText").attr("name", "q");
 }
 
-function bing(){
+function bing() {
 	document.getElementById("state").innerHTML = "<img style='width:300px;' src='https://s2.ax1x.com/2020/01/27/1ugtPA.md.png'>";
 	$('#submitButton').css('background-color', '#00868B');
 	$('#submitButton').css('color', 'white');
 	$('#Select').css('color', '#00868B');
 	$('span').css('background-color', '#00868B');
-    $('#nav').css('display', 'none');
-    $('#folder').css('display', 'block');
-    document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
-    $("#inputText").attr("placeholder","Bing搜索...");
-    $("#form").attr("action","https://cn.bing.com/search");
-    $("#inputText").attr("name","q");
+	$('#nav').css('display', 'none');
+	$('#folder').css('display', 'block');
+	document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
+	$("#inputText").attr("placeholder", "Bing搜索...");
+	$("#form").attr("action", "https://cn.bing.com/search");
+	$("#inputText").attr("name", "q");
 }
 
-function yahoo(){
+function yahoo() {
 	document.getElementById("state").innerHTML = "<img style='width:300px;' src='https://s2.ax1x.com/2020/01/27/1ughrT.md.png'>";
 	$('#submitButton').css('background-color', '#5f01d1');
 	$('#submitButton').css('color', 'white');
 	$('#Select').css('color', '#5f01d1');
 	$('span').css('background-color', '#5f01d1');
-    $('#nav').css('display', 'none');
-    $('#folder').css('display', 'block');
-    document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
-    $("#inputText").attr("placeholder","Yahoo~");
-    $("#form").attr("action","https://search.yahoo.com/search");
-    $("#inputText").attr("name","p");
+	$('#nav').css('display', 'none');
+	$('#folder').css('display', 'block');
+	document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
+	$("#inputText").attr("placeholder", "Yahoo~");
+	$("#form").attr("action", "https://search.yahoo.com/search");
+	$("#inputText").attr("name", "p");
 }
 
-function sougou(){
+function sougou() {
 	document.getElementById("state").innerHTML = "<img style='width:300px;' src='https://s2.ax1x.com/2020/01/28/1KdA8U.md.png'>";
 	$('#submitButton').css('background-color', '#f94c18');
 	$('#submitButton').css('color', 'white');
 	$('#Select').css('color', '#f94c18');
 	$('span').css('background-color', '#f94c18');
-    $('#nav').css('display', 'none');
-    $('#folder').css('display', 'block');
-    document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
-    $("#inputText").attr("placeholder","搜狗搜索...");
-    $("#form").attr("action","https://www.sogou.com/web");
-    $("#inputText").attr("name","query");
+	$('#nav').css('display', 'none');
+	$('#folder').css('display', 'block');
+	document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
+	$("#inputText").attr("placeholder", "搜狗搜索...");
+	$("#form").attr("action", "https://www.sogou.com/web");
+	$("#inputText").attr("name", "query");
 }
 
-function duckduckgo(){
+function duckduckgo() {
 	document.getElementById("state").innerHTML = "<img style='width:300px;' src='https://s2.ax1x.com/2020/01/28/1KkFVP.md.png'>";
 	$('#submitButton').css('background-color', '#de5833');
 	$('#submitButton').css('color', 'white');
 	$('#Select').css('color', '#de5833');
 	$('span').css('background-color', '#de5833');
-    $('#nav').css('display', 'none');
-    $('#folder').css('display', 'block');
-    document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
-    $("#inputText").attr("placeholder","嘎嘎嘎...");
-    $("#form").attr("action","https://duckduckgo.com/");
-    $("#inputText").attr("name","q");
+	$('#nav').css('display', 'none');
+	$('#folder').css('display', 'block');
+	document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
+	$("#inputText").attr("placeholder", "嘎嘎嘎...");
+	$("#form").attr("action", "https://duckduckgo.com/");
+	$("#inputText").attr("name", "q");
 }
 
-function magi(){
+function magi() {
 	document.getElementById("state").innerHTML = "<img style='width:300px;' src='https://s2.ax1x.com/2020/01/27/1uTCFK.md.png'>";
 	$('#submitButton').css('background-color', '#2f2f33');
 	$('#submitButton').css('color', 'white');
 	$('#Select').css('color', '#2f2f33');
 	$('span').css('background-color', '#2f2f33');
-    $('#nav').css('display', 'none');
-    $('#folder').css('display', 'block');
-    document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
-    $("#inputText").attr("placeholder","Mag[i]...");
-    $("#form").attr("action","https://magi.com/search");
-    $("#inputText").attr("name","q");
+	$('#nav').css('display', 'none');
+	$('#folder').css('display', 'block');
+	document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
+	$("#inputText").attr("placeholder", "Mag[i]...");
+	$("#form").attr("action", "https://magi.com/search");
+	$("#inputText").attr("name", "q");
 }
 
-function doge(){
+function doge() {
 	document.getElementById("state").innerHTML = "<img style='width:300px;' src='https://s2.ax1x.com/2020/01/28/1Ktf3Q.md.png'>";
 	$('#submitButton').css('background-color', '#ffca74');
 	$('#submitButton').css('color', 'black');
 	$('#Select').css('color', '#ffca74');
 	$('span').css('background-color', '#ffca74');
-    $('#nav').css('display', 'none');
-    $('#folder').css('display', 'block');
-    document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
-    $("#inputText").attr("placeholder","手动狗头.jpg");
-    $("#form").attr("action","https://www.dogedoge.com/results");
-    $("#inputText").attr("name","q");
+	$('#nav').css('display', 'none');
+	$('#folder').css('display', 'block');
+	document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
+	$("#inputText").attr("placeholder", "手动狗头.jpg");
+	$("#form").attr("action", "https://www.dogedoge.com/results");
+	$("#inputText").attr("name", "q");
 }
 
-function wechat(){
+function wechat() {
 	document.getElementById("state").innerHTML = "<img style='width:300px;' src='https://s2.ax1x.com/2020/01/28/1KwGF0.md.png'>";
 	$('#submitButton').css('background-color', '#2ca43a');
 	$('#submitButton').css('color', 'white');
 	$('#Select').css('color', '#2ca43a');
 	$('span').css('background-color', '#2ca43a');
-    $('#nav').css('display', 'none');
-    $('#folder').css('display', 'block');
-    document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
-    $("#inputText").attr("placeholder","搜微信文章...");
-    $("#form").attr("action","https://weixin.sogou.com/weixin");
-    $("#inputText").attr("name","query");
+	$('#nav').css('display', 'none');
+	$('#folder').css('display', 'block');
+	document.getElementById("Select").innerHTML = "<hr>标签 <img src='img/search-change.svg?v=2ae7ab8'>";
+	$("#inputText").attr("placeholder", "搜微信文章...");
+	$("#form").attr("action", "https://weixin.sogou.com/weixin");
+	$("#inputText").attr("name", "query");
 }

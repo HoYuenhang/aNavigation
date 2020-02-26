6e81dad7c5ca8a14f1131c69b337a7ddec358a2c
kktop <1257868332@qq.com>
2020-02-26 14:19:03 +0800

    add search engines,add loading animation,rewrite function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+/*
+ * 作者：酷安@_小K同學
+ * 项目开始日期：2020年01月26日
+ * 上次修改时间：2020年02月26日
+ * 开发日志：https://kksan.top/posts/12023/
+ *
+ * 开源相关：
+ * Github：https://github.com/Jackie1123/aNavigation
+ * CSSFX：https://cssfx.netlify.com
+ * jQuery：https://jquery.com/
+ * slideout：https://slideout.js.org/
+ * bootstrap：https://getbootstrap.com/
+ *
+ * 版权所有，请勿删除！
+*/
+
 var storage = window.localStorage;
 var data = storage.data;
 var night = storage.night;
@@ -6,7 +22,7 @@ var blockquote = $('.blockquote');
 
 if (storage.data != undefined) {
   data = data.split(',');
-  console.log(data);
+  // console.log('已存在localStorage的数据：' + data); //已存在localStorage的数据
   $('#state a img').attr('src', data[0]); //头图
   $('.submitButton').css('background-color', data[1]); //按钮bgc
   $('.submitButton').css('color', data[2]); //按钮文字color
@@ -17,6 +33,7 @@ if (storage.data != undefined) {
   $('.span').css('background-color', data[1]);
   $('.bdSug_wpr').css('box-shadow', data[1] + ' 0px 0px 5px');
 }
+
 if (storage.night != undefined) {
   night = night.split(',');
   console.log(night);
@@ -37,8 +54,65 @@ function rgb2hex(rgb) {
   }
   return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 }
+// rgb to hex结束
 
-// search
+// 添加书签
+$(function() {
+  var bookmark = {
+    data: [{
+      name: '开发日志',
+      link: 'http://kksan.top/posts/12023/',
+      icon: 'img/log.png',
+    }, {
+      name: '酷安',
+      link: 'https://www.coolapk.com',
+      icon: 'img/coolapk.png',
+    }, {
+      name: 'Via插件',
+      link: 'http://via-app.cn',
+      icon: 'img/via.png',
+    }, {
+      name: '今日热榜',
+      link: 'https://tophub.today/',
+      icon: 'img/headline.png',
+    }, {
+      name: '知乎',
+      link: 'https://www.zhihu.com/',
+      icon: 'img/zhihulogo.png',
+    }, {
+      name: 'XDA',
+      link: 'https://www.xda-developers.com/',
+      icon: 'img/xda.png',
+    }, {
+      name: '小米商城',
+      link: 'https://www.mi.com/',
+      icon: 'img/mi.png',
+    }, {
+      name: 'unsplash',
+      link: 'https://unsplash.com/',
+      icon: 'img/unsplash.png',
+    }, {
+      name: '少数派',
+      link: 'https://sspai.com/',
+      icon: 'img/sspai.png',
+    }, {
+      name: '疫情跟踪',
+      link: 'https://ncov.dxy.cn/ncovh5/view/pneumonia',
+      icon: 'img/ding.png',
+    }, {
+      name: '疫情辟谣',
+      link: 'https://vp.fact.qq.com/home',
+      icon: 'img/true.png',
+    }]
+  }
+  for (var i = 0; i < bookmark.data.length; i++) {
+    var addList = '<li class="folder-item col-xs-3 col-sm-2"> <a href="' + bookmark.data[i].link + '"><div class="folder-item-box"><img class="folder-item-img" src="' + bookmark.data[i].icon + '" /><p>' + bookmark.data[i].name + '</p></div></a></li>'
+    $('#folder ul').append(addList);
+  }
+})
+// 添加书签结束
+
+// 搜索相关
 $(function() {
   var search = {
     data: [{
@@ -121,26 +195,93 @@ $(function() {
       color: '#414baa',
       fontcolor: 'white',
       placeholder: 'private search...'
+    }, {
+      name: 'quark',
+      icon: 'img/quark-xs.png',
+      searchlink: 'https://quark.sm.cn/s',
+      searchname: 'q',
+      color: '#6182f6',
+      fontcolor: 'white',
+      placeholder: '夸克搜索...'
+    }, {
+      name: 'bilibili',
+      icon: 'img/bilibili-xs.png',
+      searchlink: 'https://search.bilibili.com/all',
+      searchname: 'keyword',
+      color: '#e47494',
+      fontcolor: 'white',
+      placeholder: 'b站是一个学习网站...'
+    }, {
+      name: 'github',
+      icon: 'img/github-xs.png',
+      searchlink: 'https://github.com/search',
+      searchname: 'q',
+      color: '#24292e',
+      fontcolor: 'white',
+      placeholder: '全球最大的开源社区...'
+    }, {
+      name: 'toutiao',
+      icon: 'img/toutiao-xs.png',
+      searchlink: 'https://m.toutiao.com/search',
+      searchname: 'keyword',
+      color: '#ed2f28',
+      fontcolor: 'white',
+      placeholder: '搜今日头条...'
+    }, {
+      name: 'weibo',
+      icon: 'img/weibo-xs.png',
+      searchlink: 'https://s.weibo.com/weibo',
+      searchname: 'q',
+      color: '#e6162d',
+      fontcolor: 'white',
+      placeholder: '搜微博...'
+    }, {
+      name: 'zhihu',
+      icon: 'img/zhihu-xs.png',
+      searchlink: 'https://www.zhihu.com/search',
+      searchname: 'q',
+      color: '#1087eb',
+      fontcolor: 'white',
+      placeholder: '我们都是有问题的人...'
     }]
   }
-  for (var i = 0; i < search.data.length; i++) {
-    var addList = '<li onclick="' + search.data[i].name + '()" class="folder-item col-xs-3 col-sm-3"> <a id="' + search.data[i].name + '" href="#"> <div class="folder-item-box"> <img class="folder-item-img" src="' + search.data[i].icon + '" /> </div> </a> </li> '
+  for (var i = 0; i < search.data.length; i++) { //添加搜索按钮
+    var addList = '<li class="folder-item col-xs-3 col-sm-2"> <a href="#"> <div class="folder-item-box"> <img id="' + search.data[i].name + '" class="folder-item-img" src="' + search.data[i].icon + '" /> </div> </a> </li> '
     $('.nav ul').append(addList);
   }
 
-  //自定义搜索引擎
-  $('.folder-item').click(function() {
-    var index = $(this).index();
-    //获取用户数据
-    var searchPho = $('#state a img').attr("src"); //搜索引擎头图
-    var color = rgb2hex($('.submitButton').css('background-color')); //搜索按钮颜色和搜索框四边颜色
-    var fontcolor = rgb2hex($('.submitButton').css('color')); //搜索按钮文字颜色
-    var searchL = $('#form').attr("action"); //searchlink
-    var searchN = $('#inputText').attr("name"); //searchname
-    var placeholder = $('#inputText').attr('placeholder');
-    // console.log(storage.data);
-    storage.data = [searchPho, color, fontcolor, placeholder, searchL, searchN]
+  // 切换搜索引擎
+  $(document).click(function(e) {
+    var id = e.target.id;
+    for (var i = 0; i < search.data.length; i++) {
+      if (id == search.data[i].name) {
+        document.getElementById("state").innerHTML = "<a href='folder://'><img style='width:300px;' src='img/" + search.data[i].name + ".png'></a>";
+        $('#submitButton').css('background-color', search.data[i].color); //按钮bg
+        $('#submitButton').css('color', search.data[i].fontcolor); //按钮
+        $('#Select').css('color', search.data[i].color); //选择器
+        $('.span').css('background-color', search.data[i].color); //输入框四边
+        $('#nav').css('display', 'none');
+        $('#folder').css('display', 'block');
+        document.getElementById("Select").innerHTML = "<hr>书签 <img src='img/search-change.svg?v=2ae7ab8'>";
+        $("#inputText").attr("placeholder", search.data[i].placeholder);
+        $("#form").attr("action", search.data[i].searchlink);
+        $("#inputText").attr("name", search.data[i].searchname);
+        $(".bdSug_wpr").css('box-shadow', search.data[i].color + ' 0px 0px 5px'); //提示框
+
+        //存入用户数据
+        var searchPho = $('#state a img').attr("src"); //搜索引擎头图
+        var color = rgb2hex($('.submitButton').css('background-color')); //搜索按钮颜色和搜索框四边颜色
+        var fontcolor = rgb2hex($('.submitButton').css('color')); //搜索按钮文字颜色
+        var searchL = $('#form').attr("action"); //searchlink
+        var searchN = $('#inputText').attr("name"); //searchname
+        var placeholder = $('#inputText').attr('placeholder');
+        storage.data = [searchPho, color, fontcolor, placeholder, searchL, searchN] //记录用户数据
+        // console.log('新存入localStorage的数据：' + storage.data); //新存入localStorage的数据
+        break;
+      }
+    }
   })
+  // 搜索相关结束
 
   // 夜间模式
   $('#night').click(function() {
@@ -172,4 +313,87 @@ $(function() {
     // storage.night = [mainbg, menubg, inner, libg, lico, blockquoteco];
     // localStorage.clear()
   })
+  // 夜间模式结束
 })
+
+//检查搜索框是否为空
+function check() {
+  var o = document.getElementById("inputText");
+  var v = o.value;
+  v = v.replace(/[ ]/g, "");
+  if (v == '') {
+    return false;
+  }
+}
+//检查搜索框是否为空结束
+
+//title问候语
+var d = new Date();
+var time = d.getHours();
+if (time < 24) {
+  document.getElementById("title").innerHTML = "一个导航 | Good evening";
+}
+if (time < 18) {
+  document.getElementById("title").innerHTML = "一个导航 | Good afternoon";
+}
+if (time < 12) {
+  document.getElementById("title").innerHTML = "一个导航 | Good morning";
+}
+if (time < 5) {
+  document.getElementById("title").innerHTML = "一个导航 | Stay up late again";
+}
+//title问候语结束
+
+//导航、引擎选择器
+function select() {
+  if ($('#folder').css('display') == 'block') {
+    $('#folder').css('display', 'none');
+    $('#nav').css('display', 'block');
+    document.getElementById("Select").innerHTML = "<hr>搜索引擎 <img src='img/search-change.svg?v=2ae7ab8'>";
+  } else if ($('#nav').css('display') == 'block') {
+    $('#nav').css('display', 'none');
+    $('#folder').css('display', 'block');
+    document.getElementById("Select").innerHTML = "<hr>书签 <img src='img/search-change.svg?v=2ae7ab8'>";
+  }
+}
+//导航、引擎选择器结束
+
+// 加载动画
+function toggle(elemt, speed) {
+  speed = speed || 16.6; //默认速度为16.6ms
+  elemt.style.display = "block"
+  if (elemt.style.opacity == 1 || elemt.style.opacity != null) {
+    let num = 20;
+    let timer = setInterval(function() {
+      num--;
+      elemt.style.opacity = num / 20;
+      if (num <= 0) {
+        clearInterval(timer);
+        elemt.style.display = "none"
+      }
+    }, speed);
+  }
+}
+
+document.onreadystatechange = function() {
+  if (document.readyState == "complete") {
+    toggle(loading, 40);
+  }
+}
+// 加载动画相关结束
+
+/*
+ * 作者：酷安@_小K同學
+ * 项目开始日期：2020年01月26日
+ * 上次修改时间：2020年02月26日
+ * 开发日志：https://kksan.top/posts/12023/
+ *
+ * 开源相关：
+ * Github：https://github.com/Jackie1123/aNavigation
+ * CSSFX：https://cssfx.netlify.com
+ * jQuery：https://jquery.com/
+ * slideout：https://slideout.js.org/
+ * bootstrap：https://getbootstrap.com/
+ *
+ * 版权所有，请勿删除！
+*/
